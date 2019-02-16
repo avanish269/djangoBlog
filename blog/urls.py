e3de99b53d396fb4479c570f7f957c40d5a14256
Avanish <avanish269@gmail.com>
2019-02-17 04:53:33 +0530

    Add, delete, publish, create draft

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name="post_detail"),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/rm/', views.post_rm, name='post_rm')
 ]
